635f284cb7b789a009c65c566f40c2c98e83a531
Felipe Coll <leonfelipecoll@gmail.com>
2025-04-19 08:35:46 -0300

    Asignacion de colores personalizados

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4f46e5",
+        secondary: "#3b82f6",
+        accent: "#9333ea",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+        mono: ["Fira Code", "monospace"],
+      },
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+        160: "40rem",
+      },
+    },
   },
   plugins: [],
 }
